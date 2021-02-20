313b9c9e999ab191da828e9d1cdd0890bfd46d64
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-02-20 22:15:21 +0900

    edit style

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -1,26 +1,17 @@
 import { readFileSync } from "fs";
-import marked from "marked";
-import { sanitizeHtml } from "./sanitizer";
 import { ParsedRequest } from "./types";
-const twemoji = require("twemoji");
-const twOptions = { folder: "svg", ext: ".svg" };
-const emojify = (text: string) => twemoji.parse(text, twOptions);
 
 const DotGothic16 = readFileSync(
   `${__dirname}/../_fonts/DotGothic16-Regular.ttf`
 ).toString("base64");
 
-function getCss(theme: string, fontSize: string) {
-  let background = "white";
-  let foreground = "black";
-  let radial = "lightgray";
-
-  if (theme === "dark") {
-    background = "black";
-    foreground = "white";
-    radial = "dimgray";
-  }
+function getCss() {
   return `
+  *,:after, :before{
+    box-sizing: border-box;
+    padding: 0;
+  margin: 0;
+  }
     @font-face {
         font-family: DotGothic16;
         src: url(data:font/woff2;charset=utf-8;base64,${DotGothic16}) format("truetype");
@@ -28,106 +19,74 @@ function getCss(theme: string, fontSize: string) {
         font-style:  normal;
     }
     body {
-        background: ${background};
-        background-image: radial-gradient(circle at 25px 25px, ${radial} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${radial} 2%, transparent 0%);
-        background-size: 100px 100px;
+        background: #272c35;
+        width: 100vw;
         height: 100vh;
         display: flex;
         text-align: center;
         align-items: center;
         justify-content: center;
+        padding: 25px;
     }
 
-    code {
-        color: #D400FF;
-        font-family: 'Vera';
-        white-space: pre-wrap;
-        letter-spacing: -5px;
-    }
-
-    code:before, code:after {
-        content: '\`';
-    }
-
-    .logo-wrapper {
+    .container {
         display: flex;
         align-items: center;
-        align-content: center;
         justify-content: center;
-        justify-items: center;
-    }
-
-    .logo {
-        margin: 0 75px;
-    }
-
-    .plus {
-        color: #BBB;
-        font-family: Times New Roman, Verdana;
-        font-size: 100px;
-    }
-
-    .spacer {
-        margin: 150px;
-    }
-
-    .emoji {
-        height: 1em;
-        width: 1em;
-        margin: 0 .05em 0 .1em;
-        vertical-align: -0.1em;
+        width: 100%;
+        height: 100%;
+        border-radius: 20px;
+        background: repeating-linear-gradient(transparent,transparent 2px,hsla(0,0%,47.8%,.9) 0,hsla(0,0%,47.8%,.9) 4px);
+        padding: 25px;
     }
     
     .heading {
         font-family: 'DotGothic16', sans-serif;
-        font-size: ${sanitizeHtml(fontSize)};
+        font-size: 100px;
         font-style: normal;
-        color: ${foreground};
+        color: #fff;
         line-height: 1.8;
-    }`;
+        position: relative;
+
+        display: -webkit-box;
+        -webkit-line-clamp: 6;
+        -webkit-box-orient: vertical;  
+        overflow: hidden;
+    }
+    .heading:before, .heading:after{
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        pointer-events: none;
+        content: attr(data-text);
+    }
+    .heading:before {
+        text-shadow: 5px 5px #ec1eff;
+        left: 0px;
+    }
+    .heading:after {
+        text-shadow: -5px 7px #31ff1e;
+        left: -1px;
+    }
+    `;
 }
 
 export function getHtml(parsedReq: ParsedRequest) {
-  const { text, theme, md, fontSize, images, widths, heights } = parsedReq;
+  const { text } = parsedReq;
   return `<!DOCTYPE html>
 <html>
     <meta charset="utf-8">
     <title>Generated Image</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
-        ${getCss(theme, fontSize)}
+        ${getCss()}
     </style>
     <body>
-        <div>
-            <div class="spacer">
-            <div class="logo-wrapper">
-                ${images
-                  .map(
-                    (img, i) =>
-                      getPlusSign(i) + getImage(img, widths[i], heights[i])
-                  )
-                  .join("")}
-            </div>
-            <div class="spacer">
-            <div class="heading">${emojify(
-              md ? marked(text) : sanitizeHtml(text)
-            )}
-            </div>
+        <div class="container">
+            <div class="heading" data-text="${text}">${text}
         </div>
     </body>
 </html>`;
-}
-
-function getImage(src: string, width = "auto", height = "225") {
-  return `<img
-        class="logo"
-        alt="Generated Image"
-        src="${sanitizeHtml(src)}"
-        width="${sanitizeHtml(width)}"
-        height="${sanitizeHtml(height)}"
-    />`;
-}
-
-function getPlusSign(i: number) {
-  return i === 0 ? "" : '<div class="plus">+</div>';
 }
